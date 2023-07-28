0088ddf8a153eb605b0d922dcd03084ba8b25c56
Theaveas So <theaveasso@theaveasso.local>
2023-07-28 16:40:17 +0700

    echo bot

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,53 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
 import Lib
+
+import           Data.Text                        (Text)
+import qualified Data.Text                        as Text
+-- import           Data.Maybe
+
+import Telegram.Bot.API
 import Telegram.Bot.Simple
+import Telegram.Bot.Simple.Debug
+import Telegram.Bot.Simple.UpdateParser
 
-data Model = Model
-data Action = DoNothing
+import System.Environment (getEnv)
+import Configuration.Dotenv (defaultConfig, loadFile)
 
-bot :: BotApp Model Action
-bot = BotApp {botJobs=_botJobs, botInitialModel=_botInitialModel, botHandler=_botHandler, botAction=_botAction}
+data Model = Model deriving (Show)
+data Action =
+    DoNothing
+  | Echo Text
+
+  deriving (Show)
+
+sdachBot :: BotApp Model Action
+sdachBot = BotApp {
+  botInitialModel = Model,
+  botAction = flip handleUpdate,
+  botHandler = handleAction,
+  botJobs = []
+}
+
+handleUpdate :: Model -> Update -> Maybe Action
+handleUpdate _ = parseUpdate
+  (Echo <$> text)
+
+handleAction :: Action -> Model -> Eff Action Model
+handleAction actions model = case actions of
+  DoNothing  -> pure model
+  Echo msg -> model <# do
+    replyText $ "sdach " `Text.append` msg
+    pure DoNothing
+    
+run :: Token -> IO ()
+run apiToken = do
+  _apiToken <- defaultTelegramClientEnv apiToken
+  startBot_ (traceBotDefault sdachBot) _apiToken
 
 main :: IO ()
-main = someFunc
+main = do
+  loadFile defaultConfig
+  token <- Token . Text.pack <$> getEnv "API_BOT_TOKEN"
+  run token
